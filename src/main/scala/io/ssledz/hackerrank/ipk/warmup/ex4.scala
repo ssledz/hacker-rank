e7b4ff4f3aa65819d9b2d9ddd5ee6ee4566a6955
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-04-07 01:26:23 +0200

    solved ex4

--- src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex4.scala
+++ src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex4.scala
@@ -5,8 +5,23 @@ package io.ssledz.hackerrank.ipk.warmup
   */
 object ex4 {
 
+  def repeatedString(s: String, n: Long): Long = {
+    val len = s.length
+    val na = s.foldLeft(0) {
+      case (acc, 'a') => acc + 1
+      case (acc, _)   => acc
+    }
+    def go(i: Int, cnt: Int, acc: Long): Long =
+      if (cnt == 0) acc
+      else go(i + 1, cnt - 1, if (s.charAt(i) == 'a') acc + 1 else acc)
+    go(0, (n % len).toInt, n / len * na)
+  }
+
   def main(args: Array[String]): Unit = {
     val lines = io.Source.stdin.getLines()
+    val s     = lines.next()
+    val n     = lines.next().toLong
+    println(repeatedString(s, n))
   }
 
 }
