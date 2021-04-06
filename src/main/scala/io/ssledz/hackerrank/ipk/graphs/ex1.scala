2a7c527644fd3ff4024d1b01b7f01230143720e5
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-04-07 01:30:36 +0200

    ...

---
+++ src/main/scala/io/ssledz/hackerrank/ipk/graphs/ex1.scala
@@ -0,0 +1,13 @@
+package io.ssledz.hackerrank.ipk.graphs
+
+/** Roads and Libraries
+  * https://www.hackerrank.com/challenges/torque-and-development
+  */
+object ex1 {
+
+  def main(args: Array[String]): Unit = {
+    val lines = io.Source.stdin.getLines()
+
+  }
+
+}
