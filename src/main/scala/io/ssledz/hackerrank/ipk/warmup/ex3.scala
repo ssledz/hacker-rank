5124975d99c88efe63ce1361a58bec3012a06c88
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-04-07 00:58:53 +0200

    solved ex3

--- src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex3.scala
+++ src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex3.scala
@@ -5,8 +5,23 @@ package io.ssledz.hackerrank.ipk.warmup
   */
 object ex3 {
 
+  def jumpingOnClouds(c: Array[Int]): Int = {
+    def go(xs: List[Int], acc: Int): Int =
+      xs match {
+        case 0 :: 0 :: 0 :: t => go(0 :: t, acc + 1)
+        case 0 :: 1 :: t      => go(t, acc + 1)
+        case 0 :: Nil         => acc
+        case 0 :: t           => go(t, acc + 1)
+      }
+    go(c.toList, 0)
+  }
+
   def main(args: Array[String]): Unit = {
     val lines = io.Source.stdin.getLines()
+    val n     = lines.next().toInt
+    val c     = lines.next().split(' ').map(_.toInt)
+
+    println(jumpingOnClouds(c))
   }
 
 }
--- src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex4.scala
+++ src/main/scala/io/ssledz/hackerrank/ipk/warmup/ex4.scala
@@ -1,5 +1,8 @@
 package io.ssledz.hackerrank.ipk.warmup
 
+/** Repeated String
+  * https://www.hackerrank.com/challenges/repeated-string/problem
+  */
 object ex4 {
 
   def main(args: Array[String]): Unit = {
